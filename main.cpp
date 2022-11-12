90bd3a6f8b6a0efebbc9c881683dfeb962c7f7fb
Eyole9 <117469913+Eyole9@users.noreply.github.com>
2022-11-11 22:01:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,39 @@
 // write your code here
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a, b, c;
+  double root, root2, discriminant;
+  root = (-b + sqrt((b * b) - 4*a*c))/2*a;
+    root2 = (-b - sqrt((b * b) - 4*a*c))/2*a;
+  discriminant= b*b-4*a*c;
+
+cout << " Enter A" << endl;
+
+  cin >> a;
+if (a == 0)  
+  { cout << "invalid entry";}
+    
+      
+  cout << endl;
+  cout << "Enter B" << endl;
+  cin >> b;
+  cout << endl;
+  cout << "Enter C" << endl;
+  cin >> c; discriminant= b*b-4*a*c;root = (-b + sqrt((b * b) - 4*a*c))/2*a;root2 = (-b - sqrt((b * b) - 4*a*c))/2*a;
+  cout << discriminant << endl;
+  cout << endl;
+  if (discriminant < 0)
+  {cout << "two complex numbers";
+  }
+  else {cout << root << ", " << root2;}
+  
+  cout << endl;
+  cout << root;
+  
+  
+  
+}
